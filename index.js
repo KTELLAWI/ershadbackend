19981b744d6d337d6100dd722ae32f363706805c
KTELLAWI <koutaiba@msn.com>
2024-11-08 07:23:28 +0000

    newdomainwith new mangodb

--- index.js
+++ index.js
@@ -18,29 +18,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors(
-     {
-     origin: ["https://literate-space-rotary-phone-94jqpx545rfp9pr-3000.app.github.dev","http://localhost:3000", "http://18.153.95.90:3000","http://tellawi.rest:3000","https://tellawi.rest"],
-     methods: ["GET", "POST", "PUT", "DELETE"],
-     credentials: true,
-     allowedHeaders: ["Content-Type", "Authorization"],
-   })
+    {
+      origin: ["https://ershad-sa.com", "https://literate-space-rotary-phone-94jqpx545rfp9pr-3000.app.github.dev", "http://localhost:3000", "http://18.153.95.90:3000", "http://tellawi.rest:3000", "https://tellawi.rest"],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
 );
 
 
 //app.use(
 //  cors()
-  //   {
-  //   origin: "https://ershad-frontend.vercel.app",
-  //   methods: ["GET", "POST", "PUT", "DELETE"],
-  //   credentials: true,
-  //   allowedHeaders: ["Content-Type", "Authorization"],
-  // }
+//   {
+//   origin: "https://ershad-frontend.vercel.app",
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true,
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// }
 //);
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
 
-res.send('welcome to Ershad App');
+  res.send('welcome to Ershad App');
 });
 
 app.use("/api/user", userRoutes);
